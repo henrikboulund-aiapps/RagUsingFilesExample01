95f79cad1193883cb321fe188f9e2368abb10214
Henrik Boulund Meng Hansen <hbh@easv.dk>
2025-04-04 10:00:33 +0200

    Removed py-file

--- OllamaEmbedder.py
+++
@@ -1,8 +0,0 @@
-from langchain_ollama import OllamaEmbeddings
-
-class OllamaEmbedder:
-
-    ollama_embedder = OllamaEmbeddings(model="nomic-embed-text")
-
-    def get_embedding(self, text):
-        return self.ollama_embedder.embed_query(text)
